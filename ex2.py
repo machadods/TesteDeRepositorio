26f9dc75fe9f7c707872b4377205fdf378a6c0c8
machadods <iavocacional@outlook.com>
2026-06-15 21:58:50 -0300

    Resolução do exercicio 2 de tabuada

--- ex2.py
+++ ex2.py
@@ -1,3 +1,10 @@
 #Exercicio 2 Tabuada
 
 
+input_num = int(input("Digite um número para ver sua tabuada: "))
+contador = 1
+while contador <= 10:
+    resultado = input_num * contador
+    print(f"{input_num} x {contador} = {resultado}")
+    contador += 1
+    
